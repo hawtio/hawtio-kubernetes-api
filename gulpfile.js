292f9fc0101cffa32fdf5a864ee805ecde96af2d
Stan Lewis <slewis@fusesource.com>
2015-09-08 15:30:10 -0400

    Bring over hawtio-kubernetes connect task

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
     path = require('path'),
     size = require('gulp-size'),
     uri = require('URIjs'),
+    urljoin = require('url-join'),
     s = require('underscore.string'),
     hawtio = require('hawtio-node-backend');
 
@@ -173,47 +174,114 @@ gulp.task('watch', ['build', 'build-example'], function() {
 
 
 gulp.task('connect', ['watch'], function() {
-  /*
-   * Example of fetching a URL from the environment, in this case for kubernetes
-  var kube = uri(process.env.KUBERNETES_MASTER || 'http://localhost:8080');
-  console.log("Connecting to Kubernetes on: " + kube);
-  */
+  // lets disable unauthorised TLS issues with kube REST API
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-  hawtio.setConfig({
-    port: 2772,
-    staticProxies: [
-    /*
-    // proxy to a service, in this case kubernetes
-    {
-      proto: kube.protocol(),
-      port: kube.port(),
-      hostname: kube.hostname(),
-      path: '/services/kubernetes',
-      targetPath: kube.path()
-    },
-    // proxy to a jolokia instance
-    {
-      proto: kube.protocol(),
-      hostname: kube.hostname(),
-      port: kube.port(),
-      path: '/jolokia',
-      targetPath: '/hawtio/jolokia'
-    }
-    */
-    ],
-    staticAssets: [{
+  var kubeBase = process.env.KUBERNETES_MASTER || 'https://localhost:8443';
+  var kube = uri(urljoin(kubeBase, 'api'));
+  var oapi = uri(urljoin(kubeBase, 'oapi'));
+  console.log("Connecting to Kubernetes on: " + kube);
+
+  var staticAssets = [{
       path: '/',
       dir: '.'
+  }];
 
-    }],
+  var dirs = fs.readdirSync('./libs');
+  dirs.forEach(function(dir) {
+    var dir = './libs/' + dir;
+    console.log("dir: ", dir);
+    if (fs.statSync(dir).isDirectory()) {
+      console.log("Adding directory to search path: ", dir);
+      staticAssets.push({
+        path: '/',
+        dir: dir
+      });
+    }
+  });
+
+  var localProxies = [];
+  var defaultProxies = [{
+    proto: kube.protocol(),
+    port: kube.port(),
+    hostname: kube.hostname(),
+    path: '/kubernetes/api',
+    targetPath: kube.path()
+  }, {
+    proto: oapi.protocol(),
+    port: oapi.port(),
+    hostname: oapi.hostname(),
+    path: '/kubernetes/oapi',
+    targetPath: oapi.path()
+  }, {
+    proto: kube.protocol(),
+    hostname: kube.hostname(),
+    port: kube.port(),
+    path: '/jolokia',
+    targetPath: '/hawtio/jolokia'
+  }, {
+    proto: kube.protocol(),
+    hostname: kube.hostname(),
+    port: kube.port(),
+    path: '/git',
+    targetPath: '/hawtio/git'
+  }];
+
+  var staticProxies = localProxies.concat(defaultProxies);
+
+  hawtio.setConfig({
+    port: process.env.DEV_PORT || 2772,
+    staticProxies: staticProxies,
+    staticAssets: staticAssets,
     fallback: 'index.html',
     liveReload: {
       enabled: true
     }
   });
-  /*
-   * Example middleware that returns a 404 for templates
-   * as they're already embedded in the js
+  var debugLoggingOfProxy = process.env.DEBUG_PROXY === "true";
+  var useAuthentication = process.env.DISABLE_OAUTH !== "true";
+
+  var googleClientId = process.env.GOOGLE_OAUTH_CLIENT_ID;
+  var googleClientSecret = process.env.GOOGLE_OAUTH_CLIENT_SECRET;
+
+  hawtio.use('/osconsole/config.js', function(req, res, next) {
+    var config = {
+      api: {
+        openshift: {
+          proto: oapi.protocol(),
+          hostPort: oapi.host(),
+          prefix: oapi.path()
+        },
+        k8s: {
+          proto: kube.protocol(),
+          hostPort: kube.host(),
+          prefix: kube.path()
+        }
+      }
+    };
+    if (googleClientId && googleClientSecret) {
+      config.master_uri = kubeBase;
+      config.google = {
+         clientId: googleClientId,
+         clientSecret: googleClientSecret,
+         authenticationURI: "https://accounts.google.com/o/oauth2/auth",
+         authorizationURI: "https://accounts.google.com/o/oauth2/auth",
+         scope: "profile",
+         redirectURI: "http://localhost:9000"
+      };
+
+    } else if (useAuthentication) {
+      config.master_uri = kubeBase;
+      config.openshift = {
+        oauth_authorize_uri: urljoin(kubeBase, '/oauth/authorize'),
+        oauth_client_id: 'fabric8'
+      };
+    }
+    var answer = "window.OPENSHIFT_CONFIG = " + stringifyObject(config);
+    res.set('Content-Type', 'application/javascript');
+    res.send(answer);
+  });
+
   hawtio.use('/', function(req, res, next) {
           var path = req.originalUrl;
           // avoid returning these files, they should get pulled from js
@@ -222,17 +290,19 @@ gulp.task('connect', ['watch'], function() {
             res.statusCode = 404;
             res.end();
           } else {
-            console.log("allowing: ", path);
+            if (debugLoggingOfProxy) {
+              console.log("allowing: ", path);
+            }
             next();
           }
         });
-        */
   hawtio.listen(function(server) {
     var host = server.address().address;
     var port = server.address().port;
     console.log("started from gulp file at ", host, ":", port);
   });
 });
+
 
 gulp.task('reload', function() {
   gulp.src('.')
